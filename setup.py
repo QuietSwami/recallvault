d475314263102b3b200795d315e0b067ad359628
Francisco Mendonca <francisco.mendonca@hesge.ch>
2024-10-14 17:22:27 +0200

    project broken

--- setup.py
+++ setup.py
@@ -1,13 +1,28 @@
 from setuptools import setup, find_packages
 
+with open("./requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
-    name='recallvault',
-    version='1.0',
+    name="recallvault",
+    version="0.1.0",
     packages=find_packages(),
-    install_requires=[],  # List of dependencies if any
+    install_requires=requirements,
     entry_points={
-        'console_scripts': [
-            'recallvault=recallvault.main:main',  # Enables running recallvault as a CLI tool
-        ]
+        "console_scripts": [
+            "recallvault=recallvault.main:cli",
+        ],
     },
+    author="Francisco Mendonça",
+    author_email="fran.abm94@gmail.com",
+    description="A quick and easy way to write project notes, diary entries, and more.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/QuietSwami/recallvault",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
